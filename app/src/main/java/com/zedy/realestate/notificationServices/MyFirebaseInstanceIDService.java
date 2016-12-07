ea8443e295bf312a765205c74a1f66ff85be43c2
mostafa_anter <ga30000@gmail.com>
2016-12-07 14:41:52 +0200

    stable_version

--- app/src/main/java/com/zedy/realestate/notificationServices/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/zedy/realestate/notificationServices/MyFirebaseInstanceIDService.java
@@ -2,8 +2,21 @@ package com.zedy.realestate.notificationServices;
 
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.zedy.realestate.activities.HomeActivity;
+import com.zedy.realestate.app.AppController;
+import com.zedy.realestate.store.RealEstatePrefStore;
+import com.zedy.realestate.utils.Constants;
+import com.zedy.realestate.utils.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by mostafa on 24/08/16.
@@ -39,7 +52,44 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
      *
      * @param token The new token.
      */
-    private void sendRegistrationToServer(String token) {
+    private void sendRegistrationToServer(final String token) {
         // TODO: Implement this method to send token to your app server.
+        if (Utils.isOnline(this)) {
+            String url = "http://real-estate.z-edy.com/api/engineer/token";
+            StringRequest jsonObjReq = new StringRequest(Request.Method.POST,
+                    url,
+                    new Response.Listener<String>() {
+
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d(TAG, response.toString());
+
+                        }
+                    }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.d(TAG, "Error: " + error.getMessage());
+                }
+            }) {
+
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("token", Constants.token);
+                    params.put("engId", new RealEstatePrefStore(MyFirebaseInstanceIDService.this)
+                            .getPreferenceValue(Constants.userId));
+                    params.put("engToken", token);
+
+                    return params;
+                }
+
+            };
+
+            // disable cache
+            jsonObjReq.setShouldCache(false);
+            // Adding request to request queue
+            AppController.getInstance().addToRequestQueue(jsonObjReq);
+        }
     }
 }
